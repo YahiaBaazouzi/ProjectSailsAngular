1296d27a16c3ee54f1bb0b2d11d56a5f43c6b697
Yahia Baazouzi <yahya-baazouzi-ess2011@live.fr>
2018-03-28 20:13:10 +0100

    first commit

---
+++ src/Scroll.js
@@ -0,0 +1,2 @@
+// When the user scrolls down 20px from the top of the document, show the button
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {NgcFloatButtonModule} from 'ngc-float-button'; 
 
 
 import { AppComponent } from './app.component';
+import { AppheaderComponent } from './components/appheader/appheader.component';
+import { AppmenuComponent } from './components/appmenu/appmenu.component';
+import { AppfooterComponent } from './components/appfooter/appfooter.component';
+import { AppsettingsComponent } from './components/appsettings/appsettings.component';
+import { EntrepriseComponent } from './esaad/entreprise/entreprise.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppheaderComponent,
+    AppmenuComponent,
+    AppfooterComponent,
+    AppsettingsComponent,
+    EntrepriseComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{path: "entreprise", component: EntrepriseComponent} ]),
+    NgcFloatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
